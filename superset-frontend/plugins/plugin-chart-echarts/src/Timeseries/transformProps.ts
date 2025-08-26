6d94e193f1f706f05328c5b84a8ea85d501c6415
flpham <florianpham@yahoo.de>
2025-08-26 08:39:19 +0200

    fix stacked charts

--- superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/transformProps.ts
+++ superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/transformProps.ts
@@ -105,6 +105,7 @@ import {
   getXAxisFormatter,
   getYAxisFormatter,
 } from '../utils/formatters';
+import { getTranslatedTimeCompare } from '../utils/series';
 
 export default function transformProps(
   chartProps: EchartsTimeseriesChartProps,
@@ -213,41 +214,13 @@ export default function transformProps(
     const match = key.match(timeCompareRegex);
     if (match) {
       const [, metric, timePart] = match;
-      let translatedTimePart;
-      switch (timePart) {
-        case '1 day ago':
-          translatedTimePart = 'Vortag';
-          break;
-        case '1 week ago':
-          translatedTimePart = 'Vorwoche';
-          break;
-        case '1 year ago':
-          translatedTimePart = 'Vorjahr';
-          break;
-        default:
-          translatedTimePart = t(timePart);
-          break;
-      }
+      let translatedTimePart = getTranslatedTimeCompare(timePart);
       verboseMap[key] = `${metric} ${translatedTimePart}`;
     } else {
       const matchWithDimensions = key.match(timeCompareRegexWithDimensions);
       if (matchWithDimensions) {
         const [, timePart, dimensions] = matchWithDimensions;
-        let translatedTimePart;
-        switch (timePart) {
-          case '1 day ago':
-            translatedTimePart = 'Vortag';
-            break;
-          case '1 week ago':
-            translatedTimePart = 'Vorwoche';
-            break;
-          case '1 year ago':
-            translatedTimePart = 'Vorjahr';
-            break;
-          default:
-            translatedTimePart = t(timePart);
-            break;
-        }
+        let translatedTimePart = getTranslatedTimeCompare(timePart);
         verboseMap[key] = `${translatedTimePart},${dimensions}`;
       }
     }
