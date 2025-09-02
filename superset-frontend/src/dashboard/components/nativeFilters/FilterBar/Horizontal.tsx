eebe5028af870ccced25aea497e3addd789d8d2a
flpham <florianpham@yahoo.de>
2025-09-02 12:18:14 +0200

    add default value

--- superset-frontend/src/dashboard/components/nativeFilters/FilterBar/Horizontal.tsx
+++ superset-frontend/src/dashboard/components/nativeFilters/FilterBar/Horizontal.tsx
@@ -40,7 +40,7 @@ import FilterBarSettings from './FilterBarSettings';
 import FilterConfigurationLink from './FilterConfigurationLink';
 import crossFiltersSelector from './CrossFilters/selectors';
 
-const formatTimeRangeText = (input: string) => {
+const formatTimeRangeText = (input: string = '') => {
   const regex = /(\d{4}-\d{2}-\d{2}) ≤ col < (\d{4}-\d{2}-\d{2})/;
   const match = input.match(regex);
   if (!match) return '';
