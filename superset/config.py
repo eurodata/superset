24bf67d06465b7c76ea7858c6dff481cb254c991
flpham <florianpham@yahoo.de>
2025-12-01 12:26:46 +0100

    add eurodata color palette

--- superset/config.py
+++ superset/config.py
@@ -656,6 +656,16 @@ COMMON_BOOTSTRAP_OVERRIDES_FUNC: Callable[  # noqa: E731
 
 # This is merely a default
 EXTRA_CATEGORICAL_COLOR_SCHEMES: list[dict[str, Any]] = []
+EXTRA_CATEGORICAL_COLOR_SCHEMES = [
+    {
+        "id": 'eurodata_1',
+        "description": '',
+        "label": 'Eurodata Standard',
+        "isDefault": True,
+        "colors":
+         ['#005c70', '#8fc7d4', '#E6BA00', '#8fc7d4', '#cc4f00',  '#005c70', '#761053']
+    }
+]
 
 # THEME_OVERRIDES is used for adding custom theme to superset
 # example code for "My theme" custom scheme
